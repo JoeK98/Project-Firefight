dfb331337502cccdbda7a5943763d09f34d476c7
Joe K <51456979+JoeK98@users.noreply.github.com>
2021-05-30 15:53:19 +0200

    Combined Fire and Water mechanic

--- Assets/Scripts/Water/JetPipeController.cs
+++ Assets/Scripts/Water/JetPipeController.cs
@@ -17,6 +17,9 @@ public class JetPipeController : WaterObjectController
     [SerializeField]
     private ConnectionController inputConnection = null;
 
+    [SerializeField]
+    private LineRenderer lineRenderer = null;
+
     // TODO: REMOVE WHEN WATER PARTICLES ARE WORKING
     [SerializeField]
     private Renderer debugRenderer = null;
@@ -27,7 +30,11 @@ public class JetPipeController : WaterObjectController
         //TODO: Only for testing purpose:
         if (OutputWaterPressure > 0.5f)
         {
+            
+
             debugRenderer.materials[2].color = Color.green;
+
+            HitFire();
         }
     }
 
@@ -36,5 +43,21 @@ public class JetPipeController : WaterObjectController
         InputWaterPressure = inputConnection.OutputWaterPressure;
 
         OutputWaterPressure = InputWaterPressure * JET_PIPE_MULTIPLIER;
+    }
+
+    private void HitFire()
+    {
+        Vector3 origin = transform.position;
+        Vector3 direction = transform.forward;
+
+        //Debug.DrawRay(origin, direction * 10f, Color.blue);
+        lineRenderer.SetPosition(0, origin);
+        lineRenderer.SetPosition(1, origin + direction * 10.0f);
+        Ray ray = new Ray(origin, direction);
+
+        if (Physics.Raycast(ray, out RaycastHit raycastHit) && raycastHit.collider.CompareTag("Fire"))
+        {
+            raycastHit.collider.GetComponent<FireRules>().fireHP -= 2;
+        }
     }
 }
