b28ac8bf8137f236e32be98502cc8d60f429161b
Joe K <51456979+JoeK98@users.noreply.github.com>
2021-07-05 19:13:43 +0200

    Optimized Textures and Materials (much better performance)

--- Assets/Scripts/SetupManager.cs
+++ Assets/Scripts/SetupManager.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 /// <summary>
 /// GameObject that sets initial values that are not available in the project settings
-/// (Currently disabling collisions between connections and "default" objects
+/// (Currently disabling collisions between connections, "default" objects and opener
 /// <author> Joe Koelbel </author>
 /// </summary>
 public class SetupManager : MonoBehaviour
@@ -10,14 +10,23 @@ public class SetupManager : MonoBehaviour
     /// <summary>
     /// The default collision layer
     /// </summary>
+    [Tooltip("The default collision layer")]
     [SerializeField]
     private int collisionLayer1 = 0;
 
     /// <summary>
     /// The connection collision layer
     /// </summary>
+    [Tooltip("The connection collision layer")]
     [SerializeField]
     private int collisionLayer2 = 1;
+
+    /// <summary>
+    /// The opener collision layer
+    /// </summary>
+    [Tooltip("The opener collision layer")]
+    [SerializeField]
+    private int collisionLayer3 = 2;
 
     /// <summary>
     /// Awake is called before the Start Method
@@ -26,5 +35,9 @@ public class SetupManager : MonoBehaviour
     {
         // Disable collisions between default and connection colliders
         Physics.IgnoreLayerCollision(collisionLayer1, collisionLayer2);
+        // Disable collisions between default and opener colliders
+        Physics.IgnoreLayerCollision(collisionLayer1, collisionLayer3);
+        // Disable collisions between connection and opener colliders
+        Physics.IgnoreLayerCollision(collisionLayer2, collisionLayer3);
     }
 }
