66f79f6c9ca4a35d6610799aca8c9e2890fe3352
Holtorf <vholtorf@gmx.de>
2021-07-06 12:43:24 +0200

    Pylon Changes
    Objectpool for the Pylon is working but needed a little fine tune

--- Assets/Scripts/PylonCloner.cs
+++ Assets/Scripts/PylonCloner.cs
@@ -4,11 +4,61 @@ using UnityEngine;
 
 public class PylonCloner : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject _pylonOriginal;
-
-    public void OnGrabCloneObject()
+    
+    [System.Serializable]
+    public class Pool
     {
-        GameObject _pylonClone = Instantiate(_pylonOriginal);
+        public string tag;
+        public GameObject pylonPrefab;
+        public int size;
     }
+
+    public List<Pool> pools;
+    private Dictionary<string, Queue<GameObject>> _poolDictionary;
+
+    void Start()
+    {
+        _poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+         foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.pylonPrefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            _poolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+
+    public GameObject GetPylonInstance()
+    {
+        foreach (KeyValuePair<string, Queue<GameObject>> entry in _poolDictionary)
+        {
+            foreach (GameObject tmp in entry.Value)
+            {
+                if (!tmp.active)
+                {
+                    tmp.SetActive(true);
+                    return tmp;
+                }
+            }
+          
+        }
+
+        return null;
+
+    }
+
+    public void OnGrabObject()
+    {
+        GetPylonInstance();
+    }
+
+
+
 }
